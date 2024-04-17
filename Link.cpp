62cb516af7dea58ce32e1c1a9765bb3a3cf64d73
Leslie Glitch <77557324+LeslieGlitch@users.noreply.github.com>
2024-04-17 15:41:27 +0000

    Add functions from PPP 15.7

--- Link.cpp
+++ Link.cpp
@@ -7,9 +7,17 @@
 */
 
 #include "Link.hpp"
+#include <iostream>
 
+// Constructor
+/// @param[in] v Value to add
+/// @param[in] p Link to predecessor
+/// @param[in] s Link to successor
+Link::Link(const std::string& v, Link* p = nullptr, Link* s = nullptr)
+    : value{v}, prev{p}, succ{s} { }
+
+// insert n before p; return n
 Link* Link::insert(Link* p, Link* n) {
-    // insert n before p; return n
     if (n==nullptr)
         return p;
     if (p==nullptr)
@@ -23,4 +31,73 @@ Link* Link::insert(Link* p, Link* n) {
     // n becomes p’s predecessor
     p->prev = n;
     return n;
+}
+
+// insert n after p; return n
+Link *Link::add(Link *p, Link *n) {
+    if (n==nullptr)
+        return p;
+    if (p==nullptr)
+        return n;
+    // p comes before n
+    n->prev = p;
+    if (p->succ)
+        p->succ->prev = n;
+    // p’s successor becomes n’s successor
+    n->succ = p->succ;
+    // n becomes p’s successor
+    p->succ = n;
+    return n;
+}
+
+// remove *p from list; return p’s successor
+Link *Link::erase(Link *p) {
+    if (p == nullptr)
+        return nullptr;
+    if (p->succ)
+        p->succ->prev = p->prev;
+    if (p->prev)
+        p->prev->succ = p->succ;
+    return p->succ;
+}
+
+// find s in list; return nullptr for “not found”
+Link *Link::find(Link *p, const std::string &s) {
+    while (p) {
+        if (p->value == s)
+            return p;
+        p = p->succ;
+    }
+    return nullptr;
+}
+
+// move n positions in list; return nullptr for “not found”
+// positive n moves forward, negative backward
+Link *Link::advance(Link *p, int n) {
+    if (p == nullptr)
+        return nullptr;
+    while (0 < n) {
+        --n;
+        if (p->succ)
+            p = p->succ;
+        return nullptr;
+    }
+    while (n < 0) {
+        ++n;
+        if (p->prev)
+            p = p->prev;
+        return nullptr;
+    }
+    return p;
+}
+
+void Link::print_all(Link* p) {
+    std::cout << "{ ";
+    while (p) {
+        std::cout << p->value;
+        if ((p=p->succ))
+            std::cout << ", ";
+    }
+    std::cout << " }";
+    return;
 }
--- Link.hpp
+++ Link.hpp
@@ -13,13 +13,34 @@
 
 class Link {
 private:
-    Link(const std::string& v, Link* p = nullptr, Link* s = nullptr)
-        : value{v}, prev{p}, succ{s} { }
     std::string value;
     Link* prev;
     Link* succ;
 public:
+    // Constructor
+    /// @param[in] v Value to add
+    /// @param[in] p Link to predecessor
+    /// @param[in] s Link to successor
+    Link(const std::string& v, Link* p, Link* s);
+
+    // insert n before p; return n
+    /// @param[in] 
     Link* insert(Link* p, Link* n);
+
+    // insert n after p; return n
+    Link *add(Link *p, Link *n);
+
+    // remove *p from list; return p’s successor
+    Link *erase(Link *p);
+
+    // find s in list; return nullptr for “not found”
+    Link *find(Link *p, const std::string &s);
+
+    // move n positions in list; return nullptr for “not found”
+    // positive n moves forward, negative backward
+    Link *advance(Link *p, int n);
+
+    void print_all(Link* p);
 };
 
 #endif
